f2bf55fbc27713d8bd8f9262eeb5094aacb370d0
Md. Rabiul Islam <rabiul77777888@gmail.com>
2025-09-18 16:29:37 +0600

    initial environment setup, navbar, footer creat

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndividualPost = () => {
+    return (
+        <div>
+            <h1>Individual Post</h1>
+        </div>
+    );
+};
+
+export default IndividualPost;
